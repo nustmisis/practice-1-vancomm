a2ca69465600b172c79396ccfb39c7caf8409e82
Ivan Peshekhonov <49730663+vancomm@users.noreply.github.com>
2023-09-19 18:08:42 +0300

    Complete task_4.py

--- practice_1/task_4.py
+++ practice_1/task_4.py
@@ -6,3 +6,33 @@
 
 @author: Savant
 """
+
+def digits(num: int, *, base: int = 10):
+    num = int(str(num)[::-1])
+    while num > 0:
+        num, mod = divmod(num, base)
+        yield mod
+
+
+def main():
+    while True:
+        text = input("Enter a 4-digit number: ")
+
+        try:
+            num = int(text)
+        except ValueError:
+            print("Error: not a number!")
+            continue
+
+        if not (1000 <= num <= 9999):
+            print("Error: not a 4-digit number")
+            continue
+
+        num_digits = list(digits(num))
+        message = f"{' + '.join(str(d) for d in num_digits)} = {sum(num_digits)}"
+
+        print(message)
+
+
+if __name__ == "__main__":
+    main()
